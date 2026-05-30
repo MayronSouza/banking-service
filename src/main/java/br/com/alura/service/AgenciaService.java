281d44d6f750a84be8c108743c8c9fb93405de32
Mayron Souza <mayron.souza@gmz.solutions>
2026-05-30 01:06:31 -0300

    criando o crud de agencia

---
+++ src/main/java/br/com/alura/service/AgenciaService.java
@@ -0,0 +1,58 @@
+package br.com.alura.service;
+
+import br.com.alura.domain.Agencia;
+import br.com.alura.domain.http.AgenciaHttp;
+import br.com.alura.domain.http.SituacaoCadastral;
+import br.com.alura.exception.AgenciaNaoAtivaOuNaoEncontrada;
+import br.com.alura.repository.AgenciaRepository;
+import br.com.alura.service.http.SituacaoCadastralHttpService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import java.util.List;
+
+@ApplicationScoped
+public class AgenciaService {
+
+    @Inject
+    @RestClient
+    private SituacaoCadastralHttpService situacaoCadastralHttpService;
+
+    private final AgenciaRepository agenciaRepository;
+
+    AgenciaService(AgenciaRepository agenciaRepository) {
+        this.agenciaRepository = agenciaRepository;
+    }
+
+    public void cadastrar(Agencia agencia) {
+        AgenciaHttp agenciaHttp = situacaoCadastralHttpService.buscarPorCnpj(agencia.getCnpj());
+
+        if(agenciaHttp != null && agenciaHttp.getSituacaoCadastral().equals(SituacaoCadastral.ATIVO)) {
+            agenciaRepository.persist(agencia);
+        } else {
+            throw new AgenciaNaoAtivaOuNaoEncontrada();
+        }
+    }
+
+    public List<Agencia> listar() {
+        return agenciaRepository.listAll();
+    }
+
+    public Agencia buscarPorId(Long id) {
+        return agenciaRepository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        agenciaRepository.deleteById(id);
+    }
+
+    public void alterar(Agencia agencia) {
+        agenciaRepository.update("nome = ?1, razaoSocial = ?2, cnpj = ?3 where id = ?4",
+                agencia.getNome(),
+                agencia.getRazaoSocial(),
+                agencia.getCnpj(),
+                agencia.getId()
+        );
+    }
+}
