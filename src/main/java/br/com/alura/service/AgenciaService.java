7dbcaed4cd344a5d0f3a29e3c1ac85ca7cf95c95
Mayron Souza <mayron.souza@gmz.solutions>
2026-05-30 19:06:28 -0300

    usando micrometer, integrando como prometheus e grafana

--- src/main/java/br/com/alura/service/AgenciaService.java
+++ src/main/java/br/com/alura/service/AgenciaService.java
@@ -6,6 +6,7 @@ import br.com.alura.domain.http.SituacaoCadastral;
 import br.com.alura.exception.AgenciaNaoAtivaOuNaoEncontrada;
 import br.com.alura.repository.AgenciaRepository;
 import br.com.alura.service.http.SituacaoCadastralHttpService;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -21,9 +22,11 @@ public class AgenciaService {
     private SituacaoCadastralHttpService situacaoCadastralHttpService;
 
     private final AgenciaRepository agenciaRepository;
+    private final MeterRegistry meterRegistry;
 
-    AgenciaService(AgenciaRepository agenciaRepository) {
+    AgenciaService(AgenciaRepository agenciaRepository, MeterRegistry meterRegistry) {
         this.agenciaRepository = agenciaRepository;
+        this.meterRegistry = meterRegistry;
     }
 
     public void cadastrar(Agencia agencia) {
@@ -31,9 +34,11 @@ public class AgenciaService {
 
         if(agenciaHttp != null && agenciaHttp.getSituacaoCadastral().equals(SituacaoCadastral.ATIVO)) {
             Log.info("A agência com CNPJ " + agencia.getCnpj() + " foi cadastrada.");
+            meterRegistry.counter("agencia_adicionada_counter").increment();
             agenciaRepository.persist(agencia);
         } else {
             Log.info("A agência com CNPJ " + agencia.getCnpj() + " não foi cadastrada.");
+            meterRegistry.counter("agencia_nao_adicionada_counter").increment();
             throw new AgenciaNaoAtivaOuNaoEncontrada();
         }
     }
